d20a910739daacbce110bb48876631ec8e7e5f23
choi0510 <yh733609@gmail.com>
2023-09-21 18:56:28 +0900

    Add Homework1.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.next();
+        int num = sc.nextInt();
+        System.out.println("문자열: " + line);
+        System.out.println("정수: " + num);
+    }
+}
